6751697f64e6280e27a429014da62ccb612b6627
Raquel Santos <39015507+RaquelMSantos@users.noreply.github.com>
2026-05-03 20:00:20 -0300

    feat(aula-01): step-02 add logs to all activity lifecycle callbacks

--- aula-01-ciclo-de-vida/app/src/main/java/com/codesbyrachel/aula01_ciclodevida/MainActivity.kt
+++ aula-01-ciclo-de-vida/app/src/main/java/com/codesbyrachel/aula01_ciclodevida/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codesbyrachel.aula01_ciclodevida
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -8,10 +9,12 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private var orderCounter = 0
+    private val TAG = "CICLO_DE_VIDA"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: Fazendo a reserva e preparando a mesa")
 
         val tvQuantity = findViewById<TextView>(R.id.tv_quantity)
         val btnAdd = findViewById<Button>(R.id.btn_add)
@@ -19,7 +22,37 @@ class MainActivity : AppCompatActivity() {
         btnAdd.setOnClickListener {
             orderCounter++
             tvQuantity.text = "$orderCounter"
+            Log.d(TAG, "Novo pedido! Total: $tvQuantity")
         }
     }
-}
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: Você chega no restaurante e ve a mesa de longe")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: Você senta e faz o pedido")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: Uma interrupção (atender o telefone)")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: Levantando da mesa e indo ao banheiro")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: Pedindo a conta e indo embora")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: volta do banheiro")
+    }
+}
